fec9ad62fc78af79557047cd9e9492fa3d4f71d9
dharmendra-jyani <dharmendra.jyani@seagate.com>
2022-02-28 04:28:06 -0700

    rgw_sal_motr:[CORTX-27698] Checksum mismatch for GET API
    Signed-off-by: dharmendra-jyani <dharmendra.jyani@seagate.com>

--- src/rgw/rgw_sal_motr.cc
+++ src/rgw/rgw_sal_motr.cc
@@ -1827,10 +1827,10 @@ int MotrObject::read_mobj(const DoutPrefixProvider* dpp, int64_t off, int64_t en
 
   left = end - off;
   for (; left > 0; off += actual) {
-    //bs may remain same, and hence this additional check
     if (left < bs)
       bs = this->get_optimal_bs(left);
     actual = bs;
+    //bs may remain same, and hence this additional check
     if (left < bs)
       actual = left;
     ldpp_dout(dpp, 20) << "MotrObject::read_mobj(): off=" << off <<
